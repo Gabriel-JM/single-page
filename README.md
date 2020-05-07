eb808a0bf3f8348911ef53c3abc417143f444921
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-05-07 00:55:40 -0300

    Refining...

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ An idea of creating a single page application only with vanilla JS.
 ### Start with.
 Run `index.html` inside a server, and have fun.
 
-### Some problems
+### Some problems:
 - It doesn't put the pages access on the browser history.
 - It doesn't put the current page path on url.
+- I have a Home page, but has only a message.
+- It allways return to the home page when reload.
--- front-end/js/app/home/home.js
+++ front-end/js/app/home/home.js
@@ -1,5 +1,15 @@
 "use strict"
 
 export default function home() {
-    document.querySelector('.home-title').innerHTML = 'Home'
+    document.querySelector('div[home]').innerHTML = (`
+        <h1 class="home-title">Wellcome to My Single Page Application.</h1>
+        <p class="made-with">Made with Vanilla JS.</p>
+        <p>Author: Gabriel José.</p>
+        <p>
+            More projects made by me in my GitHub account: 
+            <a target="_blank" href="https://github.com/Gabriel-JM">
+                My GitHub Account.
+            </a>
+        </p>
+    `)
 }
